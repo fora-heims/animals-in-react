87af758a269136d97710b61a24c4d09f39886110
fora-heims <foraheims@gmail.com>
2021-11-29 15:09:14 -0800

    map used on animals in Main. Animals reutrns all three elements, with correct props used to style the elements.

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import './Animal.css'
 
-export default function Animal() {
-  return <></>
+export default function Animal({ name, type, says, top, left }) {
+  return (
+    <div className="animal" style={{ top, left }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} alt={name}></img>
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  )
 }
